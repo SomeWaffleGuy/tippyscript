d1e66b66aff27b4e0d014a488f33e3030fb6efc0
SomeWaffleGuy <kristofergreig@gmail.com>
2020-08-29 08:13:50 -0400

    Update silverblue_3.sh

--- silverblue_3.sh
+++ silverblue_3.sh
@@ -2,6 +2,11 @@
 #TippyScript: Fedora setup script
 #CURRENT VERSION: 32
 #USE ON NEWER/OLDER VERSIONS AT OWN RISK
-rpm-ostree install steam wine-dxvk unrar cabextract fontconfig intel-media-driver
+rpm-ostree install steam wine-dxvk unrar cabextract fontconfig
+##Use for Intel GPU
+#dnf install intel-media-driver
+##Use for Nvidia GPU
+#dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver libva-utils
+#grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 exit 0
