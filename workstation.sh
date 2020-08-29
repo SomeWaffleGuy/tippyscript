7c0e264360f4533d3132d4fa344ffa53532c5713
SomeWaffleGuy <kristofergreig@gmail.com>
2020-08-29 08:20:37 -0400

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -14,7 +14,7 @@ rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-font
 ##Use for Intel GPU
 #dnf install intel-media-driver
 ##Use for Nvidia GPU
-#dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver libva-utils
+#dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver
 #grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 exit 0
