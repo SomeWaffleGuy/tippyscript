341d7ffd8018f2d371bfe97755f60d21eed6ec6b
SomeWaffleGuy <kristofergreig@gmail.com>
2020-09-09 14:58:39 -0400

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -2,16 +2,16 @@
 #TippyScript: Fedora setup script
 #CURRENT VERSION: 32
 #USE ON NEWER/OLDER VERSIONS AT OWN RISK
-sudo su -c 'dnf remove gnome-maps gnome-boxes totem gnome-photos libreoffice*'
-sudo su -c 'dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm'
-sudo su -c 'dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
-sudo su -c 'dnf groupupdate core'
-sudo su -c 'dnf upgrade'
-sudo su -c 'dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin'
-sudo su -c 'dnf groupupdate sound-and-video'
-sudo su -c 'dnf install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted'
-sudo su -c 'dnf install steam wine-dxvk unrar zip curl cabextract xorg-x11-font-utils fontconfig mpv youtube-dl mozilla-openh264 compat-ffmpeg28 ffmpeg-libs libdvdcss libva-utils ffmpegthumbnailer neofetch nextcloud-client'
-sudo su -c 'dnf install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm'
+dnf -y remove gnome-maps gnome-boxes totem gnome-photos libreoffice*
+dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y groupupdate core
+dnf -y upgrade
+dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf -y groupupdate sound-and-video
+dnf -y install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
+dnf -y install steam wine-dxvk unrar zip curl cabextract xorg-x11-font-utils fontconfig mpv youtube-dl mozilla-openh264 compat-ffmpeg28 ffmpeg-libs libdvdcss libva-utils ffmpegthumbnailer neofetch nextcloud-client
+dnf -y install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm'
 ##Use for Intel GPU
 #sudo su -c 'dnf install intel-media-driver libvdpau-va-gl'
 ##Use for Nvidia GPU
