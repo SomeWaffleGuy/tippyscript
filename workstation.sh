8b02cee41eaae43c173f5f18a8fa0246a9085e0f
SomeWaffleGuy <kristofergreig@gmail.com>
2020-09-09 16:29:20 -0400

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -2,6 +2,8 @@
 #TippyScript: Fedora setup script
 #CURRENT VERSION: 32
 #USE ON NEWER/OLDER VERSIONS AT OWN RISK
+echo "$(tput setaf 2)$(tput bold)This script will configure a fresh install of Fedora Workstation to be what I consider a useable desktop. This includes Non-Free software and drivers. If you don't like this, stop now.$(tput sgr 0)"
+sleep 3
 echo "$(tput setaf 2)$(tput bold)Uninstalling useless GNOME parts$(tput sgr 0)"
 dnf -y remove gnome-maps gnome-boxes totem gnome-photos libreoffice*
 echo "$(tput setaf 2)$(tput bold)Making sure wget and font prerequisites are installed$(tput sgr 0)"
@@ -20,11 +22,11 @@ dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=Packag
 dnf -y groupupdate sound-and-video
 echo "$(tput setaf 2)$(tput bold)Adding RPMFusion tainted repos$(tput sgr 0)"
 dnf -y install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
-dnf -y install wine-dxvk unrar zip curl celluloid youtube-dl mozilla-openh264 compat-ffmpeg28 ffmpeg-libs libdvdcss libva-utils ffmpegthumbnailer neofetch
+dnf -y install wine-dxvk unrar zip curl celluloid youtube-dl mozilla-openh264 compat-ffmpeg28 ffmpeg-libs libdvdcss libva-utils ffmpegthumbnailer neofetch gstreamer1-vaapi vulkan vulkan-loader vulkan-tools
 echo -n "$(tput setaf 2)$(tput bold)Install Steam?$(tput sgr 0) "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
-  dnf -y install steam nextcloud-client
+  dnf -y install steam
 fi
 echo -n "$(tput setaf 2)$(tput bold)Install Discord?$(tput sgr 0) "
 read answer
@@ -36,15 +38,19 @@ read answer
 if echo "$answer" | grep -iq "^y" ;then
   dnf -y install nextcloud-client
 fi
-echo -n "$(tput setaf 2)$(tput bold)Install Intel Media Driver? (VAAPI support for Intel GPUs)$(tput sgr 0) "
+echo -n "$(tput setaf 2)$(tput bold)Select your GPU
+1: Intel
+2: AMD
+3: NVIDIA
+$(tput sgr 0)"
 read answer
-if echo "$answer" | grep -iq "^y" ;then
-  dnf -y install intel-media-driver libvdpau-va-gl
-fi
-echo -n "$(tput setaf 2)$(tput bold)Install NVIDIA drivers?$(tput sgr 0) "
-read answer
-if echo "$answer" | grep -iq "^y" ;then
-  dnf -y install akmod-nvidia xorg-x11-drv-nvidia-cuda vulkan xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver
+if echo "$answer" | grep -iq "^1" ;then
+  dnf -y install intel-media-driver mesa-vulkan-drivers libvdpau-va-gl
+elif echo "$answer" | grep -iq "^2" ;then
+  dnf -y install xorg-x11-drv-amdgpu mesa-vulkan-drivers
+  echo "$(tput setaf 2)$(tput bold)Additional configuration may be required to use the AMDGPU driver. I do not have a modern AMD GPU for testing.$(tput sgr 0) "
+elif echo "$answer" | grep -iq "^3" ;then
+  dnf -y install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver
   grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
 fi
 echo "$(tput setaf 2)$(tput bold)Enabling Flathub$(tput sgr 0)"
