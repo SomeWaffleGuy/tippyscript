42fec8e6c30576100f19863973f9989fe6a6d062
SomeWaffleGuy <kristofergreig@gmail.com>
2020-09-09 14:54:36 -0400

    Delete silverblue_2.sh

--- silverblue_2.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-#TippyScript: Fedora setup script
-#CURRENT VERSION: 32
-#USE ON NEWER/OLDER VERSIONS AT OWN RISK
-rpm-ostree install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
-exit 0
