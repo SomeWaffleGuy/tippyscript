ed5f09e5149f0461a79c7276a58bb5f22da5c6d1
SomeWaffleGuy <kristofergreig@gmail.com>
2020-09-09 14:54:41 -0400

    Delete silverblue_1.sh

--- silverblue_1.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-#TippyScript: Fedora setup script
-#CURRENT VERSION: 32
-#USE ON NEWER/OLDER VERSIONS AT OWN RISK
-rpm-ostree install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-exit 0
