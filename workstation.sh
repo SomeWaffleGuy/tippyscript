8e5589949c936ab642db4042847e96b875bb57ff
SomeWaffleGuy <kristofergreig@gmail.com>
2020-09-10 00:07:26 -0400

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -3,39 +3,52 @@
 #CURRENT VERSION: 32
 #USE ON NEWER/OLDER VERSIONS AT OWN RISK
 if [[ $XDG_SESSION_TYPE =  wayland ]]; then
-echo "$(tput setaf 2)$(tput bold)Script MUST be run under Xorg. Please log out and select the GNOME on Xorg session.$(tput sgr 0) "
+echo "$(tput setaf 1)$(tput bold)Script MUST be run under Xorg. Please log out and select the GNOME on Xorg session.$(tput sgr 0) "
 exit 1
 fi
-echo "$(tput setaf 2)$(tput bold)This script will configure a fresh install of Fedora Workstation to be what I consider a useable desktop. This includes Non-Free software and drivers. For best results, run on an up-to-date system.$(tput sgr 0)"
-echo -n "$(tput setaf 2)$(tput bold)Continue? (Y/N)$(tput sgr 0) "
+echo "$(tput setaf 2)$(tput bold)This script will configure a fresh install of Fedora Workstation to be what I consider a useable desktop. This includes$(tput sgr 0)$(tput setaf 1)$(tput bold) NON-FREE SOFTWARE AND DRIVERS$(tput sgr 0)$(tput setaf 2)$(tput bold) and suggests software which may be subject to restrictions under local law. $(tput sgr 0)"
+echo -n "$(tput setaf 2)$(tput bold)Continue? 
+(y/N)$(tput sgr 0) "
 read answer
 if echo "$answer" | grep -iq "^y" ;then
-  echo "$(tput setaf 2)$(tput bold)Uninstalling useless GNOME parts$(tput sgr 0)"
+  echo "$(tput setaf 2)$(tput bold)Uninstalling useless GNOME parts...$(tput sgr 0)"
   sudo dnf -y remove gnome-maps gnome-boxes totem gnome-photos libreoffice*
-  echo "$(tput setaf 2)$(tput bold)Making sure wget and font prerequisites are installed$(tput sgr 0)"
+  echo "$(tput setaf 2)$(tput bold)Checking for system updates...$(tput sgr 0)"
+  sudo dnf -y upgrade
+  echo "$(tput setaf 2)$(tput bold)Making sure prerequisites are installed...$(tput sgr 0)"
   sudo dnf -y install wget cabextract xorg-x11-font-utils fontconfig
-  echo "$(tput setaf 2)$(tput bold)Downloading and installing RPMFusion and MS Fonts$(tput sgr 0)"
-  wget https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-  wget https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-  wget https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+  echo "$(tput setaf 2)$(tput bold)Downloading and installing RPM Fusion and MS Core Fonts...$(tput sgr 0)"
+  wget https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
   sudo dnf -y install *.rpm
   rm *.rpm
-  sudo dnf -y upgrade
-  echo "$(tput setaf 2)$(tput bold)Setting up basic multimedia functionality$(tput sgr 0)"
+  echo "$(tput setaf 2)$(tput bold)Setting up basic multimedia functionality...$(tput sgr 0)"
   sudo dnf -y groupupdate core
   sudo dnf -y upgrade
   sudo dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
   sudo dnf -y groupupdate sound-and-video
-  echo "$(tput setaf 2)$(tput bold)Adding RPMFusion tainted repos$(tput sgr 0)"
-  sudo dnf -y install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
-  echo "$(tput setaf 2)$(tput bold)Installing typical applications$(tput sgr 0)"
-  sudo dnf -y install wine-dxvk evolution gnome-tweaks gnome-shell-extension-appindicator unrar zip curl celluloid youtube-dl mozilla-openh264 compat-ffmpeg28 ffmpeg-libs libdvdcss libva-utils ffmpegthumbnailer neofetch gstreamer1-vaapi vulkan gdouros-symbola-fonts google-noto-emoji-fonts google-noto-emoji-color-fonts google-android-emoji-fonts
-  echo -n "$(tput setaf 2)$(tput bold)Install Gaming Software (Steam, Lutris, Discord)? (Y/N)$(tput sgr 0) "
+  echo "$(tput setaf 2)$(tput bold)Installing typical applications...$(tput sgr 0)"
+  sudo dnf -y install wine-dxvk evolution gnome-tweaks gnome-shell-extension-appindicator unrar zip curl celluloid youtube-dl mozilla-openh264 compat-ffmpeg28 ffmpeg-libs libva-utils ffmpegthumbnailer neofetch gstreamer1-vaapi vulkan gdouros-symbola-fonts google-noto-emoji-fonts google-noto-emoji-color-fonts google-android-emoji-fonts
+  echo -n "$(tput setaf 2)$(tput bold)Install 'tainted' RPM Fusion repositories? 
+$(tput sgr 0)$(tput setaf 1)$(tput bold)REPOS CONTAIN SOFTWARE WITH LEGAL RESTRICTIONS $(tput sgr 0)$(tput setaf 2)$(tput bold)
+(y/N)$(tput sgr 0) "
+  read answer
+  if echo "$answer" | grep -iq "^y" ;then
+    sudo dnf -y install rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
+    echo -n "$(tput setaf 2)$(tput bold)Enable DVD playback? 
+$(tput sgr 0)$(tput setaf 1)$(tput bold)LEGAL RESTRICTIONS MAY APPLY (E.g. in the USA)$(tput sgr 0)$(tput setaf 2)$(tput bold) 
+(y/N)$(tput sgr 0) "
+    if echo "$answer" | grep -iq "^y" ;then
+      sudo dnf -y install libdvdcss
+    fi
+  fi
+  echo -n "$(tput setaf 2)$(tput bold)Install Gaming Software (Steam, Lutris, Discord)? 
+(y/N)$(tput sgr 0) "
   read answer
   if echo "$answer" | grep -iq "^y" ;then
     sudo dnf -y install steam lutris discord
   fi
-  echo -n "$(tput setaf 2)$(tput bold)Install Nextcloud Client? (Y/N)$(tput sgr 0) "
+  echo -n "$(tput setaf 2)$(tput bold)Install Nextcloud Client? 
+(y/N)$(tput sgr 0) "
   read answer
   if echo "$answer" | grep -iq "^y" ;then
     sudo dnf -y install nextcloud-client
@@ -44,6 +57,7 @@ if echo "$answer" | grep -iq "^y" ;then
 1: Intel
 2: AMD
 3: NVIDIA
+4: Other/FOSS Drivers
 $(tput sgr 0)"
   read answer
   if echo "$answer" | grep -iq "^1" ;then
@@ -54,15 +68,16 @@ $(tput sgr 0)"
   elif echo "$answer" | grep -iq "^3" ;then
     sudo dnf -y install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs vdpauinfo libva-vdpau-driver
   fi
-  echo "$(tput setaf 2)$(tput bold)Enabling Flathub$(tput sgr 0)"
-  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  echo -n "$(tput setaf 2)$(tput bold)Enable S32LE in PulseAudio? (Generally fine for modern systems, skip if unsure) (Y/N)$(tput sgr 0) "
+  echo -n "$(tput setaf 2)$(tput bold)Enable S32LE in PulseAudio? (Generally fine for modern systems, skip if unsure) 
+(y/N)$(tput sgr 0) "
   read answer
   if echo "$answer" | grep -iq "^y" ;then
     sudo sed -i "s/; default-sample-format = s16le/default-sample-format = s32le/g" /etc/pulse/daemon.conf
     sudo sed -i "s/; resample-method = speex-float-1/resample-method = speex-float-10/g" /etc/pulse/daemon.conf
     sudo sed -i "s/; avoid-resampling = false/avoid-resampling = true/g" /etc/pulse/daemon.conf
   fi
+  echo "$(tput setaf 2)$(tput bold)Enabling Flathub$(tput sgr 0)"
+  sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
   echo "$(tput setaf 2)$(tput bold)Restarting GNOME Shell$(tput sgr 0)"
   busctl --user call org.gnome.Shell /org/gnome/Shell org.gnome.Shell Eval s 'Meta.restart("Restarting…")'
   sleep 3
@@ -81,7 +96,8 @@ $(tput sgr 0)"
   gsettings set org.gnome.nautilus.icon-view default-zoom-level 'standard'
   gsettings set org.gnome.gedit.preferences.editor bracket-matching 'false'
   gsettings set org.gnome.gedit.preferences.editor highlight-current-line 'false'
-  echo -n "$(tput setaf 2)$(tput bold)Use dark theme? (Y/N)$(tput sgr 0) "
+  echo -n "$(tput setaf 2)$(tput bold)Use dark theme? 
+(y/N)$(tput sgr 0) "
   read answer
   if echo "$answer" | grep -iq "^y" ;then
     gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
